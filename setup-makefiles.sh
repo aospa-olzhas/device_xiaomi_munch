1ef0089ced14ee4bdd4bcb327cf0fd4b776f400e
olzhas0986 <kdiroljas@gmail.com>
2026-07-27 09:30:27 +0500

    munch: Rewrite extraction script in bash

---
+++ setup-makefiles.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function lib_imports() {
+    cat <<EOF >>"$1"
+		"hardware/qcom-caf/common/libqti-perfd-client",
+		"hardware/qcom-caf/sm8250",
+		"hardware/xiaomi",
+		"vendor/qcom/opensource/display",
+		"vendor/xiaomi/sm8250-common",
+EOF
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=munch
+export DEVICE_COMMON=sm8250-common
+export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
+
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+
